98a03f7602106b7873f2550eaeb75bc79fa17742
Sam Shelton <sam.shelton@soloworks.co.uk>
2018-12-27 22:04:12 +0000

    Added RequestTypes

--- request.go
+++ request.go
@@ -1,5 +1,32 @@
 package alexa
 
+import "strings"
+
+// RequestType represents confirmvarious Request Types
+type RequestType int
+
+const (
+	// RequestTypeUndefined means incoming value incorrect or not supported
+	RequestTypeUndefined RequestType = iota
+	// RequestTypeLaunch is constant `LaunchRequest`
+	RequestTypeLaunch
+	// RequestTypeIntent is constant `IntentRequest`
+	RequestTypeIntent
+	// RequestTypeSessionEnded is constant `SessionEndedRequest`
+	RequestTypeSessionEnded
+	// RequestTypeCanFulfillIntent is constant `CanFulfillIntentRequest`
+	RequestTypeCanFulfillIntent
+)
+
+// requestTypeStrings for use outside this module
+var requestTypeStrings = [...]string{
+	"Undefined", // Placeholder - should never be this
+	"LaunchRequest",
+	"IntentRequest",
+	"SessionEndedRequest",
+	"CanFulfillIntentRequest",
+}
+
 // Request is an Alexa skill request
 // see https://developer.amazon.com/docs/custom-skills/request-and-response-json-reference.html#request-format
 type Request struct {
@@ -7,12 +34,33 @@ type Request struct {
 	Session Session `json:"session"`
 	Context Context `json:"context"`
 	Body    struct {
-		Type        string `json:"type"`
-		RequestID   string `json:"requestId"`
-		Timestamp   string `json:"timestamp"`
-		Locale      string `json:"locale"`
-		Intent      Intent `json:"intent,omitempty"`
-		Reason      string `json:"reason,omitempty"`
-		DialogState string `json:"dialogState,omitempty"`
+		Type        RequestType `json:"type"`
+		RequestID   string      `json:"requestId"`
+		Timestamp   string      `json:"timestamp"`
+		Locale      string      `json:"locale"`
+		Intent      Intent      `json:"intent,omitempty"`
+		Reason      string      `json:"reason,omitempty"`
+		DialogState string      `json:"dialogState,omitempty"`
 	} `json:"request"`
+}
+
+// MarshalJSON Function to handle JSON parsing out
+func (r RequestType) MarshalJSON() ([]byte, error) {
+	j := string(`"` + requestTypeStrings[r] + `"`)
+	return []byte(j), nil
+}
+
+// UnmarshalJSON Function to handle JSON parsing out
+func (r *RequestType) UnmarshalJSON(data []byte) error {
+	rt := RequestTypeUndefined
+	// Convert to string whilst removing quotes
+	x := string(data)[1 : len(data)-1]
+	// Find the type in the range of values
+	for i, s := range requestTypeStrings {
+		if strings.ToLower(s) == strings.ToLower(x) {
+			rt = RequestType(i)
+		}
+	}
+	*r = rt
+	return nil
 }
