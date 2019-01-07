9d6db41749fe38aab38d24662645adeb00b36b9f
Sam Shelton <sam.shelton@soloworks.co.uk>
2019-01-07 10:47:54 +0000

    Added Authority and Status to Resolutions

--- resolutions.go
+++ resolutions.go
@@ -3,6 +3,10 @@ package alexa
 // Resolutions is an Alexa skill Resolution
 type Resolutions struct {
 	ResolutionPerAuthority []struct {
+		Authority string `json:"authority"`
+		Status    struct {
+			Code string `json:"code"`
+		} `json:"status"`
 		Values []struct {
 			Value struct {
 				Name string `json:"name"`
